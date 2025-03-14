cd02575c039c414e4a5dea3c9b0ddf2abbf6ebf1
claudsaints <claudiodsantoss7@gmail.com>
2025-03-14 11:30:21 -0300

    Service layer

--- src/main/java/com/claudsaints/spring/resources/UserResource.java
+++ src/main/java/com/claudsaints/spring/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.claudsaints.spring.resources;
 
 import com.claudsaints.spring.entities.User;
+import com.claudsaints.spring.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"maria","maria@gmail.com","988812211","123456");
-        return  ResponseEntity.ok().body(u);
+    @Autowired
+    private UserService service;
 
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return  ResponseEntity.ok().body(list);
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return  ResponseEntity.ok().body(obj);
     }
 }
