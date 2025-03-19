3771115365ba4586924eb70e0e982e7096546f95
claudsaints <claudiodsantoss7@gmail.com>
2025-03-19 15:43:15 -0300

    Product OrderItem association

--- src/main/java/com/claudsaints/spring/entities/Product.java
+++ src/main/java/com/claudsaints/spring/entities/Product.java
@@ -17,6 +17,9 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imgUrl;
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
     @ManyToMany
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
@@ -79,6 +82,13 @@ public class Product implements Serializable {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
